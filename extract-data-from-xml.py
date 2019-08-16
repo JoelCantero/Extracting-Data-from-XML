2b1283c27ba1cef1afa3e251e7775c0b5de81547
Joel Cantero Priego <joel.cantero@est.fib.upc.edu>
2019-08-16 10:26:56 +0200

    Add files via upload

---
+++ extract-data-from-xml.py
@@ -0,0 +1,22 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location: ')
+print('Retrieving', url)
+data = urllib.request.urlopen(url, context=ctx).read()
+tree = ET.fromstring(data)
+print('Retrieved', len(data.decode()), 'characters')
+
+counts = tree.findall('.//count')
+print('Count', len(counts))
+sum = 0
+for count in counts:
+	sum = sum + int(count.text)
+print('Sum:', sum)
+# http://py4e-data.dr-chuck.net/comments_42.xml
